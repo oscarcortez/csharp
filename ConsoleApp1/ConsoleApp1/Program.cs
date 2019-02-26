a07f9b5fee208f3e8f05c855f02263f44e241c86
Oscar Cortez <Oscar.Cortez@fundacion-jala.org>
2019-02-26 16:49:15 -0400

    Primer commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyComponents;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArray arr = new MyArray(args);
+
+            if(arr.SumarArrays())
+            {
+                Console.WriteLine(arr.Show());
+            }
+            else
+                Console.WriteLine("Error en el formato de entrada");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/MyComponents/MyArray.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyComponents
+{
+    public class MyArray
+    {
+        private readonly string[] args;
+        private int[] result;
+        private int dim_result=0;
+
+        public MyArray(string[] p_args)
+        {
+            args = new string[100];
+            args = p_args;
+        }
+        public bool SumarArrays()
+        {            
+            result = new int[100];
+            
+            List<int> cont_numeros = new List<int>();
+            foreach (string arg in args)
+            {
+                int i = 0;
+                StringBuilder sb_args = new StringBuilder(arg);
+                string[] arr_args =  sb_args.ToString().Split(Convert.ToChar(","));
+                cont_numeros.Add(arr_args.Length);
+                dim_result = arr_args.Length;
+                foreach(string arr_arg in arr_args)
+                {                    
+                    if (Int32.TryParse(arr_arg, out int int_validated))
+                    {                       
+                        result[i] += int_validated;
+                        i++;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }                
+            }
+            var first_elem = cont_numeros.First();
+            foreach(int cn in cont_numeros)
+            {
+                if (first_elem != cn)
+                    return false;
+            }
+            return true;
+        }
+
+        public string Show()
+        {
+            StringBuilder str_result = new StringBuilder();
+            foreach (int res in result)
+            {
+                if (res != 0)
+                {
+                    str_result.Append(res);
+                    str_result.Append(",");
+                }
+                else
+                    break;
+            }
+            return str_result.ToString().Substring(0,str_result.Length -1);
+        }
+    }
+}
